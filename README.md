0b5c237c9515ed80bf228457759412a2b086db54
ruixu <rare0502@foxmail.com>
2024-04-24 19:50:12 +0800

    all finished

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # 常用python库的速查表
-
-收集常用python库的速查表
-[A](#a) [M](#m) [N](#n) [P](#p)
-
-## A
-## M
-## N
-## P
-![](./Pandas_Cheat_Sheet.pdf)
-## S
+    收集常用python库的速查表
---
+++ process.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+def archive_pdf():
+    # 归档到文件夹
+    pdf_file_list=os.listdir("./")
+    for item in pdf_file_list:
+        if os.path.isfile(item) and item.lower().endswith(".pdf"):
+            dir_name=item[0].upper()
+            if not os.path.exists(dir_name):
+                os.mkdir(f"./{dir_name}")
+            shutil.move(item,f"{dir_name}/{item}")
+def generate_md():
+    # 生成Markdown
+    dir_list=os.listdir("./")
+    pdf_dict={}
+    for item in dir_list:
+        if os.path.isdir(item) and item.isalpha():
+            pdf_dict[item]=os.listdir(item)
+    print(pdf_dict)
+    md_start="""# 常用python库的速查表
+    收集常用python库的速查表"""
+    md_index=""
+    for item in dir_list:
+        temp=f"[{item}](#{item})\t"
+        md_index+=temp
+        
+    md_content=""
+    f=open("README.md",mode='w',encoding='utf-8')
+    f.write(md_start+md_index+md_content)
+    f.close()
+archive_pdf()
+generate_md()
