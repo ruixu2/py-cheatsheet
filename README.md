e1230b32ae002b1caea2c955190edcc2a02b8478
ruixu <rare0502@foxmail.com>
2024-04-24 19:50:22 +0800

    generate md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # 常用python库的速查表
     收集常用python库的速查表
+[M](#M)	[N](#N)	[P](#P)	[S](#S)	
+## M
+- [matplotlib-cheatsheets.pdf](./M/matplotlib-cheatsheets.pdf)
+
+- [matplotlib-handout-beginner.pdf](./M/matplotlib-handout-beginner.pdf)
+
+- [matplotlib-handout-intermediate.pdf](./M/matplotlib-handout-intermediate.pdf)
+
+- [matplotlib-handout-tips.pdf](./M/matplotlib-handout-tips.pdf)
+
+## N
+- [NumPy-Cheat-Sheet.pdf](./N/NumPy-Cheat-Sheet.pdf)
+
+## P
+- [Pandas_Cheat_Sheet.pdf](./P/Pandas_Cheat_Sheet.pdf)
+
+## S
+- [SciPy_Cheat_Sheet.pdf](./S/SciPy_Cheat_Sheet.pdf)
+
--- process.py
+++ process.py
@@ -1,32 +1,46 @@
 import os
 import shutil
+
+
 def archive_pdf():
     # 归档到文件夹
-    pdf_file_list=os.listdir("./")
+    pdf_file_list = os.listdir("./")
     for item in pdf_file_list:
         if os.path.isfile(item) and item.lower().endswith(".pdf"):
-            dir_name=item[0].upper()
+            dir_name = item[0].upper()
             if not os.path.exists(dir_name):
                 os.mkdir(f"./{dir_name}")
-            shutil.move(item,f"{dir_name}/{item}")
+            shutil.move(item, f"{dir_name}/{item}")
+
+
 def generate_md():
     # 生成Markdown
-    dir_list=os.listdir("./")
-    pdf_dict={}
+    dir_list = os.listdir("./")
+    pdf_dict = {}
     for item in dir_list:
         if os.path.isdir(item) and item.isalpha():
-            pdf_dict[item]=os.listdir(item)
+            pdf_dict[item] = os.listdir(item)
     print(pdf_dict)
-    md_start="""# 常用python库的速查表
+    md_start = """# 常用python库的速查表
     收集常用python库的速查表"""
-    md_index=""
+    md_index = "\n"
     for item in dir_list:
-        temp=f"[{item}](#{item})\t"
-        md_index+=temp
-        
-    md_content=""
-    f=open("README.md",mode='w',encoding='utf-8')
-    f.write(md_start+md_index+md_content)
+        if os.path.isdir(item) and item.isalpha():
+            temp = f"[{item}](#{item})\t"
+            md_index += temp
+
+    md_content = "\n"
+    for key in pdf_dict.keys():
+        md_content += f"## {key}\n"
+        for item in pdf_dict[key]:
+            temp = f"- [{item}](./{key}/{item})\n\n"
+            md_content += temp
+
+    f = open("README.md", mode='w', encoding='utf-8')
+    f.write(md_start + md_index + md_content)
     f.close()
-archive_pdf()
-generate_md()
+
+
+if __name__ == '__main__':
+    archive_pdf()
+    generate_md()
